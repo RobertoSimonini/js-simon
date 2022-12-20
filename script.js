f0589aa83d030ddf3c66d76fd279bbaf5119e2cf
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-20 17:18:58 +0100

    excercise completed

--- script.js
+++ script.js
@@ -24,15 +24,48 @@ console.log('OK JS')
 */
 
 // Prendo gli elementi dal DOM 
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
 
+// Setto il setInterval per far scorrere il timer ogni secondo 
+setInterval(function() {
+    // Creo una costante la data odierna 
+    const currentDate = new Date();
+    console.log(currentDate);
 
-// Creo una costante la data odierna 
-const currentDate = new Date();
-console.log(currentDate);
+    // Creo una costante con il giorno di natale 
+    const  christmasDate = new Date(currentDate.getFullYear(), 11, 25);
 
-// Creo una costante con il giorno di natale 
-const  christmasDate = new Date(currentDate.getFullYear(), 11, 25);
+    // Calcolo i millisecondi che mi separano da oggi a Natale 
+    const secondsUntilChristmas = (christmasDate.getTime() - currentDate.getTime()) / 1000;
+
+    // Calcolo il tempo rimanente per ciascun valore
+    let days = Math.floor(secondsUntilChristmas / 86400);
+    let hours = Math.floor((secondsUntilChristmas % 86400) / 3600);
+    let minutes = Math.floor(((secondsUntilChristmas % 86400) % 3600) / 60);
+    let seconds = Math.floor(((secondsUntilChristmas % 86400) % 3600) % 60);
+
+    // Aggiungo lo 0 davanti ai valori minori di 10
+    if (days < 10) {
+        days = '0' + days;
+    }
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    // Mostro il tempo rimanente in pagina 
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    minutesElement.innerHTML = minutes;
+    secondsElement.innerHTML = seconds;
+
+}, 1000);
+  
