b469c1e6bef2d4d1275acd0bb4f492cceee75e4e
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-20 17:00:53 +0100

    took elements from dom and created first variables

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('OK JS')
+
+/*
+    # CONSEGNA
+
+    In pieno tema natalizio sfruttiamo le timing functions per  fare il conto alla rovescia per il Natale!  Ogni secondo il nostro countdown dovrà scalare fino alla mezzanotte del 25 dicembre!
+
+    Vi sono forniti in allegato HTML e CSS, cercate di analizzarli per capire come intervenire sul countdown.
+
+    Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
+    ? Da quante ore è composto un giorno?
+
+    ? Da quanti minuti è composta un'ora?
+
+    ? Da quanti secondi è composto un minuto?
+
+    ? Da quanti millisecondi è composto un secondo?
+
+    ? Quanti millisecondi mi separano dal Natale?
+
+    ? Esiste un oggetto JS in grado di gestire le date?
+
+    ?Esistono dei metodi per trasformare una data in millisecondi?
+*/
+
+// Prendo gli elementi dal DOM 
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+
+// Creo una costante la data odierna 
+const currentDate = new Date();
+console.log(currentDate);
+
+// Creo una costante con il giorno di natale 
+const  christmasDate = new Date(currentDate.getFullYear(), 11, 25);
